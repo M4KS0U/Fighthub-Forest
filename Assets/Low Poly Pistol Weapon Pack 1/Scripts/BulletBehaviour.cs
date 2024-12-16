151825a2461c0cea612f480a63111d9e5531cced
M4KS0U <maxime.eng@epitech.eu>
2024-12-17 00:48:39 +0900

    Fix temporarly bulletspawn and shoot

--- Assets/Low Poly Pistol Weapon Pack 1/Scripts/BulletBehaviour.cs
+++ Assets/Low Poly Pistol Weapon Pack 1/Scripts/BulletBehaviour.cs
@@ -1,55 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class BulletBehaviour : MonoBehaviour
-// {
-//     public int bulletDamage = 25;
-
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         print("Touche par balle " + other.name);
-
-//         if (other.CompareTag("Enemy"))
-//         {
-//             Enemy enemy = other.GetComponent<Enemy>();
-//             if (enemy != null)
-//             {
-//                 enemy.TakeDamage(bulletDamage);
-//             }
-//         }
-
-//         Destroy(gameObject);
-//     }
-// }
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class BulletBehaviour : MonoBehaviour
-// {
-//     public int bulletDamage = 25;
-
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         // Affiche dans la console l'objet touché
-//         print("Touche par balle : " + other.name);
-
-//         // Si l'objet touché a le tag "Enemy", inflige des dégâts
-//         if (other.CompareTag("Enemy"))
-//         {
-//             Enemy enemy = other.GetComponent<Enemy>();
-//             if (enemy != null)
-//             {
-//                 enemy.TakeDamage(bulletDamage);
-//             }
-//         }
-        
-//         // La balle est détruite après avoir touché un objet (qu'il soit un ennemi ou non)
-//         Destroy(gameObject);
-//     }
-// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -60,23 +8,17 @@ public class BulletBehaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Affiche dans la console l'objet touché
-        // print("Touche par balle : " + other.name);
-        print("OTUOTU");
-        // Vérifie si l'objet a un tag et s'il correspond à "Enemy"
+        print("Touche par balle : " + other.name);
         if (other.CompareTag("Enemy"))
         {
-            // Récupère le composant Enemy sur l'objet
             Enemy enemy = other.GetComponent<Enemy>();
 
-            // Si l'ennemi existe, applique des dégâts
             if (enemy != null)
             {
                 enemy.TakeDamage(bulletDamage);
             }
         }
         
-        // La balle est détruite après avoir touché un objet (qu'il soit un ennemi ou non)
-        Destroy(gameObject);
+        // Destroy(gameObject);
     }
 }
--- Assets/Low Poly Pistol Weapon Pack 1/Scripts/FireWeapon.cs
+++ Assets/Low Poly Pistol Weapon Pack 1/Scripts/FireWeapon.cs
@@ -1,53 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class FireWeapon : MonoBehaviour
-// {
-//     public GameObject bulletPrefab;
-//     public Transform bulletSpawn;
-
-//     public float bulletSpeed = 30;
-
-//     public float lifeTime = 3;
-//     void Start()
-//     {
-        
-//     }
-
-//     void Update()
-//     {
-//         if (Input.GetKeyDown(KeyCode.M))
-//         {
-//             Fire();
-//         }
-//     }
-
-//     private void Fire()
-//     {
-//         GameObject bullet = Instantiate(bulletPrefab);
-
-//         Physics.IgnoreCollision(bullet.GetComponent<Collider>(),
-//             bulletSpawn.parent.GetComponent<Collider>());
-
-//         bullet.transform.position = bulletSpawn.position;
-
-//         Vector3 rotation = bullet.transform.rotation.eulerAngles;
-
-//         bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
-        
-//         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed, ForceMode.Impulse);
-
-//         StartCoroutine(DestroyBulletAfterTime(bullet, lifeTime));
-//     }
-//     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
-//     {
-//         yield return new WaitForSeconds(delay);
-
-//         Destroy(bullet);
-//     }
-// }
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -56,38 +6,16 @@ public class FireWeapon : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
-    public Animator playerAnimator;
 
     public float bulletSpeed = 30;
+
     public float lifeTime = 3;
-
-    private int shootLayerIndex;
-
-    private void Start()
+    void Start()
     {
-        // Récupère l'index du layer "Shoot" dans l'Animator
-        if (playerAnimator != null)
-        {
-            shootLayerIndex = playerAnimator.GetLayerIndex("Shoot");
-        }
-
-        // Active le layer "Shoot" au démarrage si ce script est activé
-        EnableShootLayer(true);
+        
     }
 
-    private void OnEnable()
-    {
-        // Active le layer "Shoot" lorsque ce script est activé
-        EnableShootLayer(true);
-    }
-
-    private void OnDisable()
-    {
-        // Désactive le layer "Shoot" lorsque ce script est désactivé
-        EnableShootLayer(false);
-    }
-
-    private void Update()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
@@ -107,24 +35,96 @@ public class FireWeapon : MonoBehaviour
         Vector3 rotation = bullet.transform.rotation.eulerAngles;
 
         bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
-
+        
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed, ForceMode.Impulse);
 
         StartCoroutine(DestroyBulletAfterTime(bullet, lifeTime));
     }
-
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
     {
         yield return new WaitForSeconds(delay);
+
         Destroy(bullet);
     }
-
-    private void EnableShootLayer(bool enable)
-    {
-        if (playerAnimator != null && shootLayerIndex != -1)
-        {
-            float weight = enable ? 1f : 0f;
-            playerAnimator.SetLayerWeight(shootLayerIndex, weight);
-        }
-    }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class FireWeapon : MonoBehaviour
+// {
+//     public GameObject bulletPrefab;
+//     public Transform bulletSpawn;
+//     public Animator playerAnimator;
+
+//     public float bulletSpeed = 30;
+//     public float lifeTime = 3;
+
+//     private int shootLayerIndex;
+
+//     private void Start()
+//     {
+//         // Récupère l'index du layer "Shoot" dans l'Animator
+//         if (playerAnimator != null)
+//         {
+//             shootLayerIndex = playerAnimator.GetLayerIndex("Shoot");
+//         }
+
+//         // Active le layer "Shoot" au démarrage si ce script est activé
+//         EnableShootLayer(true);
+//     }
+
+//     private void OnEnable()
+//     {
+//         // Active le layer "Shoot" lorsque ce script est activé
+//         EnableShootLayer(true);
+//     }
+
+//     private void OnDisable()
+//     {
+//         // Désactive le layer "Shoot" lorsque ce script est désactivé
+//         EnableShootLayer(false);
+//     }
+
+//     private void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.M))
+//         {
+//             Fire();
+//         }
+//     }
+
+//     private void Fire()
+//     {
+//         GameObject bullet = Instantiate(bulletPrefab);
+
+//         Physics.IgnoreCollision(bullet.GetComponent<Collider>(),
+//             bulletSpawn.parent.GetComponent<Collider>());
+
+//         bullet.transform.position = bulletSpawn.position;
+
+//         Vector3 rotation = bullet.transform.rotation.eulerAngles;
+
+//         bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+
+//         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward * bulletSpeed, ForceMode.Impulse);
+
+//         StartCoroutine(DestroyBulletAfterTime(bullet, lifeTime));
+//     }
+
+//     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+//     {
+//         yield return new WaitForSeconds(delay);
+//         Destroy(bullet);
+//     }
+
+//     private void EnableShootLayer(bool enable)
+//     {
+//         if (playerAnimator != null && shootLayerIndex != -1)
+//         {
+//             float weight = enable ? 1f : 0f;
+//             playerAnimator.SetLayerWeight(shootLayerIndex, weight);
+//         }
+//     }
+// }
